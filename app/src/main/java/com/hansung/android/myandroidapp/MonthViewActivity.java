05acac439742e8a41668861ca0d55d4b99c97be2
Young-Jae-Lim <80191322+Young-Jae-Lim@users.noreply.github.com>
2021-03-31 16:42:56 +0900

    년/월 표시 및 이전,다음버튼 완료

--- app/src/main/java/com/hansung/android/myandroidapp/MonthViewActivity.java
+++ app/src/main/java/com/hansung/android/myandroidapp/MonthViewActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -23,19 +26,32 @@ public class MonthViewActivity extends AppCompatActivity {
         year=intent.getIntExtra("year",-1); //인텐트값 받기
         month=intent.getIntExtra("month", -1);
 
+
         //       Calendar cal = Calendar.getInstance();
         //       cal.set(2021,2,1);
         //       int dayofweek = cal.get(Calendar.DAY_OF_WEEK);
 
-        if(year == -1 || month == -1) {
+
+        //초기 현재 날자값 받아오기
+        if(year == -1 && month == -1) {
             year = Calendar.getInstance().get(Calendar.YEAR);
             month = Calendar.getInstance().get(Calendar.MONTH);
         }
+        //1월달이면 month 의 값이 0이고 만약 month -1이면 yaer의 값 1감소하고 month를 다시12월부터 시작
+        else if(month == -1){
+            year = year -1;
+            month = 11;
+        }
+        //12월달이면 month 의 값이 11이고 만약 month 12이면 yaer의 값 1증가하고 month를 다시1월부터 시작
+        else if(month == 12){
+            year = year+1;
+            month = 0;
+        }
 
         TextView yearmonthTv =findViewById(R.id.month);
-        yearmonthTv.setText(year+"년"+month+ "월");
+        yearmonthTv.setText(year+"년"+(month+1)+ "월");
 
-        Button privBtn = findViewById(R.id.btn);
+        Button privBtn = findViewById(R.id.pre);
         privBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,6 +62,24 @@ public class MonthViewActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        Button nextBtn = findViewById(R.id.next);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MonthViewActivity.class);
+                intent.putExtra("year",year);
+                intent.putExtra("month",month+1);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        String[] day = {"일","월","화","수","목","금","토"};
+        ArrayAdapter<String> dayadapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,day);
+        GridView daygrid = (GridView) findViewById(R.id.dayofweekgridview);
+        daygrid.setAdapter(dayadapt);
     }
 
 
